2776655c71053f443db9e3b982c83f80c1eb4cc0
GwangCheonLee <raitmid56@gmail.com>
2024-12-12 22:28:09 +0900

    chore: remove batch and config files

--- src/main/java/com/example/springbatch/band/repositories/BandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbatch.band.repositories;
-
-import com.example.springbatch.band.entities.Band;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BandRepository extends JpaRepository<Band, Long> {
-}
